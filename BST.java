89726bb175be76235a6e3dec4ba89d81bf69830b
Caius <jfklaszky@gmail.com>
2016-07-13 14:47:40 -0400

    node is just about done and search seems to be working, but needs more testing

--- BST.java
+++ BST.java
@@ -1,2 +1,56 @@
 public class BST
-{return;}
+{
+	private BST_Node root;
+
+	public BST()
+	{
+		this.root = null;
+	}
+
+	public BST(int data)
+	{
+		this.root = new BST_Node(data);
+	}
+
+	public void setRoot(BST_Node node)
+	{
+		this.root = node;
+	}
+
+	public BST_Node getRoot()
+	{
+		return root;
+	}
+
+	public static boolean search(int data, BST_Node rt)
+	{
+		if(rt.getData() == data)
+		{
+			return true;
+		}
+		else
+		{
+			if(data > rt.getData() && rt.getRight() != null)
+			{
+				return search(data, rt.getRight());
+			}
+			else if(data < rt.getData() && rt.getLeft() != null)
+			{
+				return search(data, rt.getLeft());
+			}
+		}
+
+		return false;
+
+	}
+	public static insert(int data, BST_Node root)
+	{
+		
+	}
+
+	public static void main(String[] args)
+	{
+		BST root = new BST(200);
+		System.out.println(search(200, root.root));
+	}
+}
--- BST_Node.java
+++ BST_Node.java
@@ -1,11 +1,41 @@
-public class BST_Node<T>
+public class BST_Node
 {
 	private BST_Node left;
 	private BST_Node right;
-	private T data;
+	private int data;
 
-	public BST_Node(T data)
+	public BST_Node(int data)
 	{
 		this.data = data;
+	}
+
+	public void setLeft(BST_Node left)
+	{
+		this.left = left;
+	}
+
+	public void setRight(BST_Node right)
+	{
+		this.right = right;
+	}
+
+	public void setData(int data)
+	{
+		this.data = data;
+	}
+
+	public BST_Node getLeft()
+	{
+		return this.left;
+	}
+
+	public BST_Node getRight()
+	{
+		return this.right;
+	}
+
+	public int getData()
+	{
+		return this.data;
 	}
 }
